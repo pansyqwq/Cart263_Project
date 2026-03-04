7ffddfd5ccea3a57638461c7ab7b0df3020ec165
BenWoschi <benjaminw4101@gmail.com>
2026-03-03 22:13:32 -0500

    Began layout for project
    added html elements and css

---
+++ 263-Midterm/script.js
@@ -0,0 +1,17 @@
+const audio = document.getElementById("audio");
+const playPauseBtn = document.getElementById("playPause");
+const volumeSlider = document.getElementById("volumeSlider");
+
+playPauseBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.textContent = "Stop";
+  } else {
+    audio.pause();
+    playPauseBtn.textContent = "Play";
+  }
+});
+
+volumeSlider.addEventListener("input", () => {
+  audio.volume = volumeSlider.value;
+});
