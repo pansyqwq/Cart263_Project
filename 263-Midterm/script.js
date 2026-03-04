1e0ec66250a737ed8b2ab14bb3d3fc9a52110dc6
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-03-04 01:46:50 -0500

    this part is bugging so i comment it out

--- 263-Midterm/script.js
+++ 263-Midterm/script.js
@@ -2,15 +2,15 @@ const audio = document.getElementById("audio");
 const playPauseBtn = document.getElementById("playPause");
 const volumeSlider = document.getElementById("volumeSlider");
 
-playPauseBtn.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.play();
-    playPauseBtn.textContent = "Stop";
-  } else {
-    audio.pause();
-    playPauseBtn.textContent = "Play";
-  }
-});
+// playPauseBtn.addEventListener("click", () => {
+//   if (audio.paused) {
+//     audio.play();
+//     playPauseBtn.textContent = "Stop";
+//   } else {
+//     audio.pause();
+//     playPauseBtn.textContent = "Play";
+//   }
+// });
 
 volumeSlider.addEventListener("input", () => {
   audio.volume = volumeSlider.value;
